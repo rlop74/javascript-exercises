6c2ea80661098c0299e2b8dc94ea768213d87fc6
rlop74 <119091421+rlop74@users.noreply.github.com>
2024-01-07 16:43:52 -0500

    Solved exercises 04 and 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let myNums = [];
+    let sum = 0;
 
+    if (typeof start === "number" && typeof end === "number") {
+        if (start > 0 && end > 0) {
+            if (start < end) {
+                for (let i = start; i <= end; i++) {
+                    myNums.push(i);
+                }
+            } else if (start > end) {
+                for (let i = start; i >= end; i--) {
+                    myNums.push(i);
+                }
+            }
+        } else return "ERROR";
+    } else return "ERROR";
+
+    myNums.forEach(element => { sum += element;    
+    });
+    return sum;
 };
+
+
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
