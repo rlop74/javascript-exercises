f4994ee85c2f218e327d67446c71a3ce76615179
rlop74 <119091421+rlop74@users.noreply.github.com>
2024-01-07 17:51:27 -0500

    Added modified files (was already resolved)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...arg) {
+    const newArray = [];
+    
+    array.forEach(element => {
+        if (!arg.includes(element))
+        newArray.push(element);
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
